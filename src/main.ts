483e608123b0d882ceaee9692f1468178d11f773
promzybankz <promzybankz@gmail.com>
2025-10-03 18:27:06 +0100

    Uploaded Persistent

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import router from './router'
 import './assets/main.css'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import { createPersistedState } from 'pinia-plugin-persistedstate'
+// import { createPersistedState } from 'pinia-plugin-persistedstate'
 import PrimeVue from 'primevue/config'
 import Aura from '@primeuix/themes/aura'
 import VueAwesomePaginate from 'vue-awesome-paginate'
@@ -17,12 +17,12 @@ import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
 
-pinia.use(
-  createPersistedState({
-    storage: localStorage,
-    key: (id) => `__persisted__${id}`,
-  }),
-)
+// pinia.use(
+//   createPersistedState({
+//     storage: localStorage,
+//     key: (id) => `__persisted__${id}`,
+//   }),
+// )
 
 const app = createApp(App)
 
